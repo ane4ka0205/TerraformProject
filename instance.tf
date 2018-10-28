d8bfeaf83d45483839bfe7321270152dedb30fd1
Anna Mlanaova <aamalanova@gmail.com>
2018-10-28 22:22:42 +0000

    fixed all

--- instance.tf
+++ instance.tf
@@ -1,9 +1,19 @@
+#web
 resource "aws_instance" "instance1" {
-  ami                    = "ami-01d5b8c6e4958a724"
+  ami                    = "${lookup(var.amis, var.region)}"
   instance_type          = "t2.micro"
   key_name               = "project1"
   subnet_id              = "${aws_subnet.artemis-public.id}"
   vpc_security_group_ids = ["${aws_security_group.artemis.id}"]
   user_data              = "${file("apache.sh")}"
   associate_public_ip_address = "true"
+}
+#DB host
+resource "aws_instance" "instance2" {
+  ami                    = "${lookup(var.amis, var.region)}"
+  instance_type          = "t2.micro"
+  key_name               = "project1"
+  subnet_id              = "${aws_subnet.artemis-private.id}"
+  vpc_security_group_ids = ["${aws_security_group.artemis.id}"]
+  user_data              = "${file("db.sh")}"
 }
