440c6df8a303c155ca5ba61ca458a0b92042b592
Anna Malanova <42782581+ane4ka0205@users.noreply.github.com>
2018-11-03 11:15:18 -0500

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "instance1" {
   ami                    = "${lookup(var.amis, var.region)}"
   instance_type          = "t2.micro"
-  key_name               = "project1"
+  key_name               = "jenkins"
   subnet_id              = "${aws_subnet.artemis-public.id}"
   vpc_security_group_ids = ["${aws_security_group.artemis.id}"]
   user_data              = "${file("apache.sh")}"
@@ -18,7 +18,7 @@ resource "aws_instance" "instance1" {
 resource "aws_instance" "instance2" {
   ami                    = "${lookup(var.amis, var.region)}"
   instance_type          = "t2.micro"
-  key_name               = "project1"
+  key_name               = "jenkins"
   subnet_id              = "${aws_subnet.artemis-private.id}"
   vpc_security_group_ids = ["${aws_security_group.artemis.id}"]
   user_data              = "${file("db.sh")}"
